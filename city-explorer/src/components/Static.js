7c2568fd94324dd4594284985822f39485dae9dd
Storm OBryant <vmarie1997@gmail.com>
2022-08-29 10:07:24 -0500

    cleaned up, readme, modularized-ish

--- city-explorer/src/components/Static.js
+++ city-explorer/src/components/Static.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react'
 
-// import { Modal, Button } from 'react-bootstrap'
-
 export default class Static extends Component {
-    // constructor(props) {
-    //   super(props)
-    
-    //   this.state = {
-    //     show: false,
-    //   }
-    // }
-
-    // handleClose = () =>{
-    //     this.setState({show: false})
-    //     this.props.closeModalFromCards();
-    // }
-
-    render() {
-    return (
-        <div style={{display: 'flex', flexDirection:'column', paddingBottom: '2rem'}}>
-            <h5>Map for {this.props.title.charAt(0).toUpperCase() + this.props.title.slice(1)}</h5>
-            <img style={{height: '20rem', width: '18rem', borderRadius: '5%', margin: 'auto'}} alt='a map' src={this.props.src}/>
-        </div>
-    )
-    }
+	render() {
+		return (
+			<div style={{ display: 'flex', flexDirection: 'column', paddingBottom: '2rem' }}>
+				<h5>Map for {this.props.title.charAt(0).toUpperCase() + this.props.title.slice(1)}</h5>
+				<img style={{ height: '20rem', width: '18rem', borderRadius: '5%', margin: 'auto' }} alt='a map' src={this.props.src} />
+			</div>
+		)
+	}
 }
